8f254653b16fa3eff1dfd9fcba4deb2f820c77e2
King2481 <bjcrum94@gmail.com>
2021-02-12 10:42:47 -0600

    Dashing implemented

--- Source/Gridiron/Characters/GridironMovementComponent.cpp
+++ Source/Gridiron/Characters/GridironMovementComponent.cpp
@@ -2,8 +2,118 @@
 
 
 #include "Gridiron/Characters/GridironMovementComponent.h"
+#include "Gridiron/Characters/GridironCharacter.h"
 
 UGridironMovementComponent::UGridironMovementComponent()
 {
 	OwningCharacter = nullptr;
+	
+	RequestToStartDash = false;
+	GroundDashMultiplier = 2.f;
+	AirbornDashMultiplier = 1.1f;
+}
+
+void UGridironMovementComponent::StartDash()
+{
+	RequestToStartDash = true;
+
+	Velocity = FVector::ZeroVector;
+
+	FVector LaunchVelocity = Acceleration;
+	LaunchVelocity.Z = 0.f;
+	LaunchVelocity *= IsFalling() ? AirbornDashMultiplier : GroundDashMultiplier;
+
+	Launch(LaunchVelocity);
+}
+
+void UGridironMovementComponent::EndDash()
+{
+	RequestToStartDash = false;
+}
+
+void UGridironMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+
+	//The Flags parameter contains the compressed input flags that are stored in the saved move.
+	//UpdateFromCompressed flags simply copies the flags from the saved move into the movement component.
+	//It basically just resets the movement component to the state when the move was made so it can simulate from there.
+	RequestToStartDash = (Flags & FSavedMove_Character::FLAG_Custom_0) != 0;
+}
+
+FNetworkPredictionData_Client * UGridironMovementComponent::GetPredictionData_Client() const
+{
+	check(PawnOwner != NULL);
+
+	if (!ClientPredictionData)
+	{
+		UGridironMovementComponent* MutableThis = const_cast<UGridironMovementComponent*>(this);
+
+		MutableThis->ClientPredictionData = new FGridironNetworkPredictionData_Client(*this);
+		MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
+		MutableThis->ClientPredictionData->NoSmoothNetUpdateDist = 140.f;
+	}
+
+	return ClientPredictionData;
+}
+
+void UGridironMovementComponent::FGridironSavedMove::Clear()
+{
+	Super::Clear();
+
+	SavedRequestToStartDash = false;
+}
+
+uint8 UGridironMovementComponent::FGridironSavedMove::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if (SavedRequestToStartDash)
+	{
+		Result |= FLAG_Custom_0;
+	}
+
+	return Result;
+}
+
+bool UGridironMovementComponent::FGridironSavedMove::CanCombineWith(const FSavedMovePtr & NewMove, ACharacter * Character, float MaxDelta) const
+{
+	//Set which moves can be combined together. This will depend on the bit flags that are used.
+	if (SavedRequestToStartDash != ((FGridironSavedMove*)&NewMove)->SavedRequestToStartDash)
+	{
+		return false;
+	}
+
+	return Super::CanCombineWith(NewMove, Character, MaxDelta);
+}
+
+void UGridironMovementComponent::FGridironSavedMove::SetMoveFor(ACharacter * Character, float InDeltaTime, FVector const & NewAccel, FNetworkPredictionData_Client_Character & ClientData)
+{
+	Super::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
+
+	UGridironMovementComponent* CharacterMovement = Cast<UGridironMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+		SavedRequestToStartDash = CharacterMovement->RequestToStartDash;
+	}
+}
+
+void UGridironMovementComponent::FGridironSavedMove::PrepMoveFor(ACharacter * Character)
+{
+	Super::PrepMoveFor(Character);
+
+	UGridironMovementComponent* CharacterMovement = Cast<UGridironMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+	}
+}
+
+UGridironMovementComponent::FGridironNetworkPredictionData_Client::FGridironNetworkPredictionData_Client(const UCharacterMovementComponent& ClientMovement)
+	: Super(ClientMovement)
+{
+}
+
+FSavedMovePtr UGridironMovementComponent::FGridironNetworkPredictionData_Client::AllocateNewMove()
+{
+	return FSavedMovePtr(new FGridironSavedMove());
 }
--- Source/Gridiron/Characters/GridironMovementComponent.h
+++ Source/Gridiron/Characters/GridironMovementComponent.h
@@ -16,6 +16,42 @@ class GRIDIRON_API UGridironMovementComponent : public UCharacterMovementCompone
 {
 	GENERATED_BODY()
 
+	class FGridironSavedMove : public FSavedMove_Character
+	{
+	public:
+
+		typedef FSavedMove_Character Super;
+
+		///@brief Resets all saved variables.
+		virtual void Clear() override;
+
+		///@brief Store input commands in the compressed flags.
+		virtual uint8 GetCompressedFlags() const override;
+
+		///@brief This is used to check whether or not two moves can be combined into one.
+		///Basically you just check to make sure that the saved variables are the same.
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* Character, float MaxDelta) const override;
+
+		///@brief Sets up the move before sending it to the server. 
+		virtual void SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character & ClientData) override;
+		///@brief Sets variables on character movement component before making a predictive correction.
+		virtual void PrepMoveFor(class ACharacter* Character) override;
+
+		// Dash
+		uint8 SavedRequestToStartDash : 1;
+	};
+
+	class FGridironNetworkPredictionData_Client : public FNetworkPredictionData_Client_Character
+	{
+	public:
+		FGridironNetworkPredictionData_Client(const UCharacterMovementComponent& ClientMovement);
+
+		typedef FNetworkPredictionData_Client_Character Super;
+
+		///@brief Allocates a new copy of our custom saved move
+		virtual FSavedMovePtr AllocateNewMove() override;
+	};
+
 public:
 
 	UGridironMovementComponent();
@@ -23,5 +59,22 @@ public:
 	// Cache of the owner of this movement component;
 	UPROPERTY(BlueprintReadOnly, Category = "Movement Component")
 	AGridironCharacter* OwningCharacter;
+
+	uint8 RequestToStartDash : 1;
+
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+	virtual class FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+
+	UFUNCTION(BlueprintCallable, Category = "Character Movement: Dash")
+	void StartDash();
+
+	UFUNCTION(BlueprintCallable, Category = "Character Movement: Dash")
+	void EndDash();
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Character Movement: Dash")
+	float GroundDashMultiplier;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Character Movement: Dash")
+	float AirbornDashMultiplier;
 	
 };
